5e922bac31718b11aa6887d6be54e10234f3bc4f
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-18 21:57:56 +0530

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
-  }
+}
 
-
-  class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-      ),
+      theme: MyTheme.lightTheme,
+      darkTheme: MyTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
-        "/" : (context)=> LoginPage(),
+        "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
-        MyRoutes.loginRoute: (context) =>  LoginPage()
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,8 +8,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
-        backgroundColor: Colors.deepPurple,
+        title: Text(
+          "Catalog App",
+      ),
       ),
         body: Center(
           child: Container(
