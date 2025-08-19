0cdcd96bb6c688e2bdc2cbabd7399c9fa19184e4
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-19 22:46:16 +0530

    Day 13 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,20 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class CatalogModel{
-  static final items = [Item(
-  id: 1,
-  name: "iPhone 12 Pro",
-  description: "Apple iPhone 12th generation",
-  price: 999,
-  color: "#33505a",
-  imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"
-)
-];
+  static List<Item> items = [];
 }
 
 
 
 
 
-class Item{
+class Item {
   final int id;
   final String name;
   final String description;
@@ -30,4 +25,76 @@ class Item{
     required this.color,
     required this.imageUrl,
   });
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? description,
+    num? price,
+    String? color,
+    String? imageUrl,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      imageUrl: imageUrl ?? this.imageUrl,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'color': color,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      description: map['desc'] as String,
+      price: map['price'] as num,
+      color: map['color'] as String,
+      imageUrl: map['image'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, description: $description, price: $price, color: $color, imageUrl: $imageUrl)';
+  }
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.name == name &&
+      other.description == description &&
+      other.price == price &&
+      other.color == color &&
+      other.imageUrl == imageUrl;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      description.hashCode ^
+      price.hashCode ^
+      color.hashCode ^
+      imageUrl.hashCode;
+  }
 }
