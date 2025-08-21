59b5e4430fbb6d1f456fc612f6ba9c9a85d3ec12
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-21 14:29:36 +0530

    Day 20 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -3,6 +3,13 @@ import 'dart:convert';
 
 class CatalogModel{
   static List<Item> items = [];
+
+  // Get item by ID
+  static Item getById(int id) =>
+      items.firstWhere((element) => element.id == id, orElse: null);
+
+  // Get item by position
+  static Item getByPosition(int pos) => items[pos];
 }
 
 
