54214034ea24b6045c6ca2b3db12bea18755992c
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-21 14:14:13 +0530

    Day 19 completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyTheme {
   static ThemeData get lightTheme => ThemeData(
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.poppins().fontFamily,
+        cardColor: Colors.white,
+        canvasColor: creamColor,
         appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0,
@@ -19,7 +22,7 @@ class MyTheme {
       );
 
   static ThemeData get darkTheme => ThemeData(
-        brightness: Brightness.dark,
+      brightness: Brightness.dark,
       );
 
       //Colors
