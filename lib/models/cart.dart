c5e39c071c2eefc2a7c427fa33c7c883e47f9bb7
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-21 18:58:53 +0530

    Day 22 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_catalog/models/catalog.dart';
+
+class CartModel{
+
+  // Catalog field
+  late CatalogModel _catalog;
+
+  // Collection of item IDs - store Ids of each item
+  final List<int> _itemIds = [];
+
+  // Get catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  // Get item in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  // Get total price
+  num get totalPrice => items.fold(0, (total, current) => total + current.price);
+
+  // Add item to the cart
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  // Remove item from the cart
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
