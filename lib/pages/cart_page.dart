17f243bc7045374e326b0d269e96f9baac40c203
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-21 22:11:41 +0530

    Day 26 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -27,7 +28,7 @@ class _CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _cart = CartModel();
+    final CartModel _cart = (VxState.store as MyStore).cart;
     return SizedBox(
       height: 200,
       child: Row(
@@ -51,9 +52,10 @@ class _CartTotal extends StatelessWidget {
 }
 
 class _CartList extends StatelessWidget{
-  final _cart = CartModel();
+  
   @override
   Widget build(BuildContext context) {
+    final CartModel _cart = (VxState.store as MyStore).cart;
     return _cart.items.isEmpty? "No items in cart".text.xl3.makeCentered(): ListView.builder(
       itemCount: _cart.items.length,
       itemBuilder: (context, index) => ListTile(
--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
@@ -7,32 +8,37 @@ import 'package:velocity_x/velocity_x.dart';
 
 class AddToCart extends StatelessWidget {
   final Item catalog;
-   AddToCart({
-    super.key,
-    required this.catalog,
-  });
-  
-  final _cart = CartModel();
+
+  AddToCart({super.key, required this.catalog});
+
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(catalog) ?? false;
-    return ElevatedButton(
-      onPressed: () {
-          
-          if (!isInCart) {
-            isInCart = isInCart.toggle();
-          final _catalog = CatalogModel();
-          
-          _cart.catalog = _catalog;
-          _cart.add(catalog);
-          // setState(() {});
-          }
+    final CartModel _cart = (VxState.store as MyStore).cart;
+
+    return VxBuilder<MyStore>(
+      mutations: {AddMutation},
+      builder: (context, store, status) {
+        bool isInCart = _cart.items.contains(catalog);
+
+        return ElevatedButton(
+          onPressed: () {
+            if (!isInCart) {
+              AddMutation(catalog);
+            }
+          },
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(MyTheme.darkBluishColor),
+            shape: MaterialStateProperty.all(StadiumBorder()),
+          ),
+          child: isInCart
+              ? const Icon(Icons.done)
+              : const Icon(
+                  CupertinoIcons.cart_badge_plus,
+                  color: Colors.white,
+                ),
+        );
       },
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(MyTheme.darkBluishColor),
-        shape: MaterialStateProperty.all(StadiumBorder()),
-      ),
-      child: isInCart ? Icon(Icons.done) : Icon(CupertinoIcons.cart_badge_plus,color: Colors.white,),
     );
   }
 }
