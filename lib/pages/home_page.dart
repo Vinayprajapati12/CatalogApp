1a66d61b7bc3802a8d06387bcfac1df04ac9d74f
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-23 00:27:06 +0530

    Day 28 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,12 +3,15 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_catalog/core/store.dart';
+import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:flutter_catalog/widgets/home_widgets/catalog_header.dart';
 import 'package:flutter_catalog/widgets/home_widgets/catalog_list.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -18,7 +21,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
    final int days = 30;
 
-   final String name = "Vinay";
+  final String name = "Vinay Prajapati";
+
+  // final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
 
    @override
   void initState() {
@@ -39,14 +44,27 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
+    final _cart = (VxState.store as MyStore).cart;
     return  Scaffold(
       backgroundColor: MyTheme.creamColor,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-        backgroundColor: MyTheme.darkBluishColor,
-        child: Icon(CupertinoIcons.cart,color: Colors.white,),
+      floatingActionButton: VxBuilder(
+  mutations: {AddMutation, RemoveMutation},
+  builder: (context, store, status) {
+    return FloatingActionButton(
+      onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+      backgroundColor: MyTheme.darkBluishColor,
+      child: Icon(CupertinoIcons.cart, color: Colors.white),
+    ).badge(
+      color: Vx.red500,
+      size: 22,
+      count: _cart.items.length,
+      textStyle: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
       ),
+    );
+  },
+),
         body: SafeArea(
           child: Container(
             padding: Vx.m32,
--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -32,7 +32,7 @@ class AddToCart extends StatelessWidget {
             shape: MaterialStateProperty.all(StadiumBorder()),
           ),
           child: isInCart
-              ? const Icon(Icons.done)
+              ? const Icon(Icons.done, color: Colors.white)
               : const Icon(
                   CupertinoIcons.cart_badge_plus,
                   color: Colors.white,
